3d1e7b49e419e12c08c6721ecc32bf093c0b0437
Viktor Peretiatko <viktor_peretiatko@epam.com>
2015-04-27 12:18:16 +0300

    Fix   log/error handling logic  after review

--- src/main/java/name/webdizz/sonar/grammar/issue/tracking/LinkFunction.java
+++ src/main/java/name/webdizz/sonar/grammar/issue/tracking/LinkFunction.java
@@ -10,6 +10,7 @@ import org.sonar.api.ServerExtension;
 import org.sonar.api.config.Settings;
 import org.sonar.api.issue.action.Function;
 
+import javax.xml.ws.WebServiceException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -27,10 +28,9 @@ public class LinkFunction implements Function, ServerExtension {
     @Override
     public void execute(Context context) {
         String word = getMistakeWord(context.issue().message(), PluginParameter.ERROR_DESCRIPTION);
-        LOGGER.info("try to write:" + word);
         Settings settings = context.projectSettings();
         String dict = stripToEmpty(settings.getString(PluginParameter.MANUAL_DICT));
-        saveProperties(dict + "," + word, PluginParameter.MANUAL_DICT, context);
+        saveProperties(dict, word, PluginParameter.MANUAL_DICT, context);
     }
 
     private String getMistakeWord(String message, String errDescription) {
@@ -38,7 +38,7 @@ public class LinkFunction implements Function, ServerExtension {
         return res.substring(0, res.length() - 1);
     }
 
-    private void saveProperties(String word, String dictName, Context context) {
+    private void saveProperties(String dictionary, String word, String dictName, Context context) {
         try {
             String hostPort = checkNotNull(getProperty(context, GrammarPlugin.HOST_PORT));
             String credentials = checkNotNull(getProperty(context, GrammarPlugin.CREDENTIAL));
@@ -46,19 +46,18 @@ public class LinkFunction implements Function, ServerExtension {
             String[] location = hostPort.split(":");
             Preconditions.checkArgument(location.length == 2, "Illegal sonar location format in grammar settings");
 
-            HttpURLConnection connection = getConnection(word, dictName, credentials, location);
+            HttpURLConnection connection = getConnection(dictionary + "," + word, dictName, credentials, location);
             InputStream content = connection.getInputStream();
             BufferedReader in = new BufferedReader(new InputStreamReader(content));
-            LOGGER.info("Saving word response:" + in.readLine());
+            LOGGER.info("Save word: {} ; response: {}", word, in.readLine());
         } catch (IOException e) {
-            LOGGER.error("Cannot connect to Sonar for updating dictionary." + e.getMessage());
-            throw new RuntimeException("Cannot save word to dictionary");
+            throw new WebServiceException("Cannot connect to Sonar for updating dictionary." + e.getMessage());
         }
     }
 
     private HttpURLConnection getConnection(String word, String dictName, String credentials, String[] location) throws IOException {
         String urlString = String.format("http://%s:%s/api/properties?id=%s&value=%s", location[0], location[1], dictName, word);
-        LOGGER.info("URL:" + urlString);
+        LOGGER.debug("URL:" + urlString);
         URL url = new URL(urlString);
 
         Base64 b = new Base64();
