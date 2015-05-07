95b1f455f0f06675ce856b9d1e2d78729c20c830
Izzet Mustsfaiev <webdizz@gmail.com>
2015-05-07 17:59:01 +0300

    Add some corrections in namings

--- src/main/java/name/webdizz/sonar/grammar/GrammarPlugin.java
+++ src/main/java/name/webdizz/sonar/grammar/GrammarPlugin.java
@@ -20,7 +20,8 @@ import name.webdizz.sonar.grammar.utils.SpellCheckerUtil;
 
 public class GrammarPlugin extends SonarPlugin {
 
-
+    private static final String SPELL_CHECKER_CATEGORY = "Spell Checker";
+    private static final String DICTIONARY_CATEGORY = "Dictionary";
 
     @Override
     public List getExtensions() {
@@ -44,41 +45,48 @@ public class GrammarPlugin extends SonarPlugin {
 
                         PropertyDefinition.builder(PluginParameter.SPELL_MINIMUMWORDLENGTH).name("Minimum word length")
                                 .type(PropertyType.INTEGER).description("Defines minimum word length to analyse")
-                                .defaultValue("3").subCategory("Spell Checker").build(),
+                                .defaultValue("3").subCategory(SPELL_CHECKER_CATEGORY).build(),
+
                         PropertyDefinition.builder(PluginParameter.SPELL_IGNOREMIXEDCASE).name("Ignore mixed case")
                                 .type(PropertyType.BOOLEAN).description("Words that have mixed case are not spell " +
-                                "checked, example: 'SpellChecker'").defaultValue("false").subCategory("Spell Checker")
+                                "checked, example: 'SpellChecker'").defaultValue("false").subCategory(SPELL_CHECKER_CATEGORY)
                                 .build(),
+
                         PropertyDefinition.builder(PluginParameter.SPELL_IGNOREUPPERCASE).name("Ignore uppercase")
                                 .type(PropertyType.BOOLEAN).description("Words that are all upper case are not spell " +
-                                "checked, example: 'CIA'").defaultValue("true").subCategory("Spell Checker").build(),
+                                "checked, example: 'CIA'").defaultValue("true").subCategory(SPELL_CHECKER_CATEGORY).build(),
+
                         PropertyDefinition.builder(PluginParameter.SPELL_IGNOREDIGITWORDS)
                                 .name("Ignore words with digits").type(PropertyType.BOOLEAN)
                                 .description("Words that have digits in them are not spell checked, example: 'mach5'")
-                                .defaultValue("false").subCategory("Spell Checker").build(),
+                                .defaultValue("false").subCategory(SPELL_CHECKER_CATEGORY).build(),
+
                         PropertyDefinition.builder(PluginParameter.SPELL_IGNOREINTERNETADDRESSES)
                                 .name("Ignore words like internet address").type(PropertyType.BOOLEAN)
                                 .description("Words that look like an Internet address are not spell checked, example:" +
-                                        " 'http://www.google.com'").defaultValue("true").subCategory("Spell Checker")
+                                        " 'http://www.google.com'").defaultValue("true").subCategory(SPELL_CHECKER_CATEGORY)
                                 .build(),
                         PropertyDefinition.builder(PluginParameter.SPELL_THRESHOLD).name("Threshold value")
                                 .type(PropertyType.INTEGER).description("The maximum cost of suggested spelling. Any " +
                                 "suggestions that cost more are thrown away").defaultValue("1")
-                                .subCategory("Spell Checker").build(),
+                                .subCategory(SPELL_CHECKER_CATEGORY).build(),
 
 
-                        PropertyDefinition.builder(PluginParameter.EXCLUSION).name("Exclusion Property")
+                        PropertyDefinition.builder(PluginParameter.EXCLUSION).name("Exclusions")
                                 .type(PropertyType.STRING).description("Defines resources to be excluded from analysis")
-                                .subCategory("Dictionary").defaultValue("").build(),
-                        PropertyDefinition.builder(PluginParameter.INCLUSION).name("Inclusion Property")
+                                .subCategory(DICTIONARY_CATEGORY).defaultValue("").build(),
+
+                        PropertyDefinition.builder(PluginParameter.INCLUSION).name("Inclusions")
                                 .type(PropertyType.STRING).description("Defines resources to be included for analysis")
-                                .subCategory("Dictionary").defaultValue("").build(),
+                                .subCategory(DICTIONARY_CATEGORY).defaultValue("").build(),
+
                         PropertyDefinition.builder(PluginParameter.DICTIONARY_PATH).name("Default dictionary path")
                                 .type(PropertyType.STRING).description("Defines default dictionary path")
-                                .defaultValue("/dict/english.0").subCategory("Dictionary").build(),
+                                .defaultValue("/dict/english.0").subCategory(DICTIONARY_CATEGORY).build(),
+
                         PropertyDefinition.builder(PluginParameter.ALTERNATIVE_DICTIONARY_PROPERTY_KEY)
                                 .name("Alternative dictionary").type(PropertyType.STRING).description("Alternative " +
-                                "dictionary").defaultValue("").subCategory("Dictionary").build()
+                                "dictionary").defaultValue("").subCategory(DICTIONARY_CATEGORY).build()
                 );
     }
 }
