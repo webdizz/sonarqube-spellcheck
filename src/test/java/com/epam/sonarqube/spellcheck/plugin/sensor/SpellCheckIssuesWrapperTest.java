925d9d4c5ec39b8bdc34922e6bf6b6c5bcb383d4
GurVic <v.l.sorok@gmail.com>
2015-09-30 12:09:07 +0300

    cover class with tests

--- src/test/java/com/epam/sonarqube/spellcheck/plugin/sensor/SpellCheckIssuesWrapperTest.java
+++ src/test/java/com/epam/sonarqube/spellcheck/plugin/sensor/SpellCheckIssuesWrapperTest.java
@@ -1,16 +1,22 @@
 package com.epam.sonarqube.spellcheck.plugin.sensor;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
-import static org.mockito.Mockito.anyDouble;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.*;
 
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.LoggingEvent;
+import ch.qos.logback.core.Appender;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.slf4j.Logger;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.slf4j.LoggerFactory;
 import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.component.ResourcePerspectives;
@@ -21,14 +27,15 @@ import org.sonar.api.rule.RuleKey;
 /**
  * Unit test for GrammarIssuesWrapper
  */
+@RunWith(MockitoJUnitRunner.class)
 public class SpellCheckIssuesWrapperTest {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SpellCheckIssuesWrapperTest.class);
-
+    private Logger logger;
+    private Appender mockAppender = mock(Appender.class);
+    @Captor
+    private ArgumentCaptor<LoggingEvent> captorLoggingEvent;
     private final InputFile inputFile = mock(InputFile.class);
-
     private final ResourcePerspectives perspectives = mock(ResourcePerspectives.class);
-
     private final RuleKey ruleKey = mock(RuleKey.class);
 
     private final SpellCheckIssuesWrapper wrapper = SpellCheckIssuesWrapper.builder()
@@ -39,12 +46,42 @@ public class SpellCheckIssuesWrapperTest {
             .setRuleKey(ruleKey)
             .build();
 
-    public SpellCheckIssuesWrapperTest() {
+    @Before
+    public void setup() {
+        logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.addAppender(mockAppender);
+    }
 
+    @After
+    public void teardown() {
+        logger = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
+        logger.detachAppender(mockAppender);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldThrowExceptionWhenBuildWrapperWithNullInputFile() {
+        SpellCheckIssuesWrapper testWrapper = SpellCheckIssuesWrapper.builder()
+                .setInputFile(null)
+                .setLine("Test line")
+                .setLineNumber(1)
+                .setPerspectives(perspectives)
+                .setRuleKey(ruleKey)
+                .build();
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void shouldThrowExceptionWhenBuildWrapperOnEmptyLine() {
+        SpellCheckIssuesWrapper testWrapper = SpellCheckIssuesWrapper.builder()
+                .setInputFile(inputFile)
+                .setLine("")
+                .setLineNumber(1)
+                .setPerspectives(perspectives)
+                .setRuleKey(ruleKey)
+                .build();
     }
 
     @Test
-    public void testGetLine() {
+    public void shouldProperlyBuildWrapperFromSourceWrapperAndReturnLine() {
         String expResult = "ABCDEF";
         SpellCheckIssuesWrapper instance
                 = SpellCheckIssuesWrapper.builder(wrapper).setLine(expResult).build();
@@ -53,7 +90,7 @@ public class SpellCheckIssuesWrapperTest {
     }
 
     @Test
-    public void testGetLineNumber() {
+    public void shouldProperlyConstructWrapperWithSourceWrapperAndNewLineNumber() {
         int expResult = 100;
         SpellCheckIssuesWrapper instance
                 = SpellCheckIssuesWrapper.builder(wrapper).setLineNumber(expResult).build();
@@ -62,9 +99,10 @@ public class SpellCheckIssuesWrapperTest {
     }
 
     @Test
-    public void testIncident_String_int() {
+    public void shouldProperlyConstructWrapperAndIncidentAndWriteDebugInfo() {
         String message = "ABCD";
         int column = 10;
+        logger.setLevel(Level.DEBUG);
         ResourcePerspectives mockPerspectives = mock(ResourcePerspectives.class);
         Issuable issuable = mock(Issuable.class);
         Issuable.IssueBuilder builder = mock(Issuable.IssueBuilder.class);
@@ -93,20 +131,25 @@ public class SpellCheckIssuesWrapperTest {
         verify(builder).message(message);
         verify(builder).attribute(org.mockito.Matchers.anyString(), eq(Integer.toString(column)));
         verify(issuable).addIssue(issue);
+
+        verify(mockAppender, atLeastOnce()).doAppend(captorLoggingEvent.capture());
+        final LoggingEvent loggingEvent = captorLoggingEvent.getValue();
+        assertThat(loggingEvent.getLevel(), is(Level.DEBUG));
     }
 
     @Test
-    public void testBuilder_0args() {
+    public void shouldConstructDifferentBuildersFromEmptyWrapper() {
         SpellCheckIssuesWrapper.Builder expResult = SpellCheckIssuesWrapper.builder();
         SpellCheckIssuesWrapper.Builder result = SpellCheckIssuesWrapper.builder();
         assertNotEquals(expResult, result);
     }
 
     @Test
-    public void testBuilder_GrammarIssuesWrapper() {
+    public void shouldConstructDifferentBuilderFromSameWrappersAndWithoutLogginInfo() {
+        logger.setLevel(Level.OFF);
         SpellCheckIssuesWrapper.Builder expResult = SpellCheckIssuesWrapper.builder(wrapper);
         SpellCheckIssuesWrapper.Builder result = SpellCheckIssuesWrapper.builder(wrapper);
         assertNotEquals(expResult, result);
+        verify(mockAppender, never()).doAppend(captorLoggingEvent.capture());
     }
-
 }
